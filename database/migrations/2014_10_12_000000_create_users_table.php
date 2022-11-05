661d32f0c7080317c196ae1d8c62607a7f7bdb33
SpyBooster - Niel <38681682+Spybooster@users.noreply.github.com>
2022-11-05 11:34:08 +0800

    wip

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->text('address');
+            $table->string('contact_number');     
+            $table->text('thumbnail')->nullable();     
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username'); 
             $table->string('password');
+            $table->string('type')->default('student');
+            $table->string('status')->default('ongoing');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

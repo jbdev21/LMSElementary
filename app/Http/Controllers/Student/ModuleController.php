ae8a616e51c886352158914bf31cbc56d80d1c4d
SpyBooster - Niel <38681682+Spybooster@users.noreply.github.com>
2022-11-08 11:44:05 +0800

    wip

--- app/Http/Controllers/Student/ModuleController.php
+++ app/Http/Controllers/Student/ModuleController.php
@@ -11,11 +11,7 @@ use Illuminate\Support\Facades\Auth;
 
 class ModuleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function index(Request $request)
     {
         //auth user module
@@ -26,67 +22,41 @@ class ModuleController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Module  $module
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function show(Module $module)
     {
-        //
+        $files = $module->getMedia('files');
+
+
+        return view('student.module.show', [
+            'module' => $module,
+            'files' => $files
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Module  $module
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Module $module)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Module  $module
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function update(Request $request, Module $module)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Module  $module
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy(Module $module)
     {
         //

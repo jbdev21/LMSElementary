fee469ec8c18b97541123143970025c9830569cc
Jofie Bernas <bernaswebdevelopment@gmail.com>
2023-03-09 00:01:41 +0800

    wip

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,23 +8,58 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
-// import Echo from 'laravel-echo';
+/* ===== Responsive Sidepanel ====== */
+const sidePanelToggler = document.getElementById('sidepanel-toggler'); 
+const sidePanel = document.getElementById('app-sidepanel');  
+const sidePanelDrop = document.getElementById('sidepanel-drop'); 
+const sidePanelClose = document.getElementById('sidepanel-close'); 
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+window.addEventListener('load', function(){
+	responsiveSidePanel(); 
+});
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.addEventListener('resize', function(){
+	responsiveSidePanel(); 
+});
+
+
+function responsiveSidePanel() {
+    let w = window.innerWidth;
+	if(w >= 1200) {
+	    // if larger 
+	    //console.log('larger');
+		sidePanel.classList.remove('sidepanel-hidden');
+		sidePanel.classList.add('sidepanel-visible');
+		
+	} else {
+	    // if smaller
+	    //console.log('smaller');
+	    sidePanel.classList.remove('sidepanel-visible');
+		sidePanel.classList.add('sidepanel-hidden');
+	}
+};
+
+sidePanelToggler.addEventListener('click', () => {
+	if (sidePanel.classList.contains('sidepanel-visible')) {
+		console.log('visible');
+		sidePanel.classList.remove('sidepanel-visible');
+		sidePanel.classList.add('sidepanel-hidden');
+		
+	} else {
+		console.log('hidden');
+		sidePanel.classList.remove('sidepanel-hidden');
+		sidePanel.classList.add('sidepanel-visible');
+	}
+});
+
+
+
+sidePanelClose.addEventListener('click', (e) => {
+	e.preventDefault();
+	sidePanelToggler.click();
+});
+
+sidePanelDrop.addEventListener('click', (e) => {
+	sidePanelToggler.click();
+});
